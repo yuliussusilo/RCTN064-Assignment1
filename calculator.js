2d460aa611e651d71073aa4197e80641b0bfc9e0
Yulius Susilo <susilo.yulius@gmail.com>
2022-08-29 21:35:35 +0700

    Calculator

--- calculator.js
+++ calculator.js
@@ -45,7 +45,7 @@ class Calculator {
         case '*':
           computation = prev * current
           break
-        case '÷':
+        case '/':
           computation = prev / current
           break
         default:
